af6286963f74561bfae90db80b790881b142ac35
lindeneg <christian@lindeneg.org>
2022-01-16 22:02:27 +0100

    update globals

--- globals.ts
+++ globals.ts
@@ -1,6 +1,7 @@
 export namespace globals {
   export const yourName = "Christian Lindeneg";
   export const siteName = "lindeneg";
+  export const twitterHandle = "";
   export const siteDescription = "I like to write TypeScript & .NET programs";
   export const siteCreationDate = "January 16, 2020 04:00:00 GMT";
   export const email = "hi@lindeneg.org";
