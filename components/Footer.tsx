a7fde3474c9c4b09d4b8f4e0a405402b508b615b
lindeneg <christian@lindeneg.org>
2022-02-26 14:07:19 +0100

    give credit to colin and devii

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,17 @@ import { globals } from '../globals';
 
 export const Footer: React.FC = () => (
   <div className="footer">
-    <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
+    <div className="wrapper">
+      <p>{`© ${globals.yourName} ${new Date().getFullYear()}`}</p>
+      <p>|</p>
+      <a href="https://github.com/colinhacks/devii" target="_blank">
+        Made with: <span>Devii</span>
+      </a>
+      <p>|</p>
+      <a href="https://github.com/colinhacks" target="_blank">
+        Credit to: <span>colinhacks</span>
+      </a>
+    </div>
     <a href="/rss.xml">
       <img src="/img/rss-white.svg" alt="RSS Feed" height="30" width="30" />
     </a>
